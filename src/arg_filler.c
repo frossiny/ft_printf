5eb3108ebbafa2fbcc6c23b52a393c3f6c69038f
Florian ROSSINY <frossiny@student.42.fr>
2019-01-17 14:52:24 +0100

    New arg_filler

--- src/arg_filler.c
+++ src/arg_filler.c
@@ -6,25 +6,44 @@
 /*   By: frossiny <frossiny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 14:50:59 by frossiny          #+#    #+#             */
-/*   Updated: 2019/01/15 17:06:21 by frossiny         ###   ########.fr       */
+/*   Updated: 2019/01/17 14:51:21 by frossiny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	fill_arg(t_arg *alst, va_list *args)
+void	fill_decimal(t_arg *alst, va_list *args)
 {
-	if (!alst || !args)
-		return ;
 	if (alst->type == 'd' || alst->type == 'i')
 	{
 		alst->data.ll = va_arg(*args, long long);
 		if (alst->size == none)
 			alst->data.ll = (int)alst->data.ll;
+		else if (alst->size == l)
+			alst->data.ll = (long)alst->data.ll;
+		else if (alst->size == h)
+			alst->data.ll = (short)alst->data.ll;
+		else if (alst->size == hh)
+			alst->data.ll = (char)alst->data.ll;
+		return ;
 	}
-	else if (alst->type == 'u' || alst->type == 'o' || alst->type == 'x'
-			|| alst->type == 'X' || alst->type == 'b')
-		alst->data.ull = va_arg(*args, unsigned long long);
+	alst->data.ull = va_arg(*args, unsigned long long);
+	if (alst->size == none)
+		alst->data.ll = (unsigned int)alst->data.ll;
+	else if (alst->size == l)
+		alst->data.ll = (unsigned long)alst->data.ll;
+	else if (alst->size == h)
+		alst->data.ll = (unsigned short)alst->data.ll;
+	else if (alst->size == hh)
+		alst->data.ll = (unsigned char)alst->data.ll;
+}
+
+void	fill_arg(t_arg *alst, va_list *args)
+{
+	if (!alst || !args || alst->type == '\0')
+		return ;
+	if (ft_strchr("bdiouxX", alst->type) != NULL)
+		fill_decimal(alst, args);
 	else if (alst->type == 'f')
 	{
 		if (alst->size == L)
